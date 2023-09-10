fd6266c80c8a2c354b4be78e9b615919880e803f
FarahNurFadhilah <140325614+FarahNurFadhilah@users.noreply.github.com>
2023-09-10 20:02:29 +0700

    versi 1.1 (js)
    variabel & kostanta

---
+++ js/variabel.js
@@ -0,0 +1,19 @@
+let angka;
+
+console.log(angka);
+
+let kata = "saya belajar";
+
+const data = 100;
+
+angka = 5;
+
+console.log(angka);
+console.log(kata);
+console.log(data);
+
+angka = 8;
+console.log(angka);
+
+let isi = "saya";
+console.log(typeof isi);
